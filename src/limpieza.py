c5d80ce680092ebf4a2dec8ed42dc7c076bd11e0
Alvaro <alvarodavidguevara@gmail.com>
2026-05-15 12:04:03 -0500

    feat: Agregar visualización para de datos para el data set

--- src/limpieza.py
+++ src/limpieza.py
@@ -4,4 +4,25 @@ ruta = "data/Connacionales_inscritos_en_el_Registro_Ciudadano_en_Línea_20260514
 
 df = pd.read_excel(ruta)
 
-print(df.columns)
+# Ver primeras filas
+print(df.head())
+print("------------------------------")
+
+# Ver información general
+print(df.info())
+print("------------------------------")
+
+# Ver dimensiones
+print("Dimension del dataset")
+print(df.shape)
+print("------------------------------")
+
+# Ver datos nulos
+print("Datos Nulos: ")
+print(df.isnull().sum())
+print("------------------------------")
+
+# Ver datos Duplicados
+print("Datos Duplicados: ")
+print(df.duplicated().sum())
+print("------------------------------")
